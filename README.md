9d5f293a4ede186fb28231dee4c937ea36794446
Ntetikova <Ntetikova@gmail.com>
2023-04-30 17:46:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# PostmanEcho
+
+[![Build status](https://ci.appveyor.com/api/projects/status/t6q62471qjai248a?svg=true)](https://ci.appveyor.com/project/Ntetikova/api-ci-vpnm6)
+
---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,29 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(403)
+                .contentType( ContentType.JSON)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+}
